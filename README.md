b3213d6323859cc2f1e44b4a7254ec3ba2ba5863
Clue Mediator <cluemediator@gmail.com>
2020-12-21 13:27:39 +0530

    implement demo

---
+++ README.md
@@ -0,0 +1,21 @@
+# stripe-payment-gateway-nodejs
+Confirm a stripe paymentIntent using Node.js
+
+## Documentation
+
+[https://www.cluemediator.com/confirm-a-stripe-paymentintent-using-node-js](https://www.cluemediator.com/confirm-a-stripe-paymentintent-using-node-js)
+
+## Quick Start
+
+Follow the below steps to run the project.
+
+1. Clone repository
+2. Run `npm i` command to install dependencies
+3. Execute `node server.js` command to run the project
+
+## Connect with us
+
+Website: [Clue Mediator](https://www.cluemediator.com)  
+Like us on [Facebook](https://www.facebook.com/thecluemediator)  
+Follow us on [Twitter](https://twitter.com/cluemediator)  
+Join us on [Telegram](https://t.me/cluemediator)
---
+++ server.js
@@ -0,0 +1,59 @@
+var express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const stripe = require('stripe')('<YOUR_SECRET_KEY>');
+
+var app = express();
+var port = process.env.PORT || 4000;
+
+// enable CORS
+app.use(cors());
+// parse application/json
+app.use(bodyParser.json());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// confirm the paymentIntent
+app.post('/pay', async (request, response) => {
+  try {
+    // Create the PaymentIntent
+    let intent = await stripe.paymentIntents.create({
+      payment_method: request.body.payment_method_id,
+      description: "Test payment",
+      amount: request.body.amount * 100,
+      currency: 'inr',
+      confirmation_method: 'manual',
+      confirm: true
+    });
+    // Send the response to the client
+    response.send(generateResponse(intent));
+  } catch (e) {
+    // Display error on client
+    return response.send({ error: e.message });
+  }
+});
+
+const generateResponse = (intent) => {
+  if (intent.status === 'succeeded') {
+    // The payment didn’t need any additional actions and completed!
+    // Handle post-payment fulfillment
+    return {
+      success: true
+    };
+  } else {
+    // Invalid status
+    return {
+      error: 'Invalid PaymentIntent status'
+    };
+  }
+};
+
+// request handlers
+app.get('/', (req, res) => {
+  res.send('Stripe Integration! - Clue Mediator');
+});
+
+app.listen(port, () => {
+  console.log('Server started on: ' + port);
+});
